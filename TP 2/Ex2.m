e303cdc53a6cd786a3b7d32325a63f73ece01509
omar-ahmd <omaharmad@gmail.com>
2021-01-20 13:19:37 +0200

    FM and PM Demodulation

---
+++ TP 2/Ex2.m
@@ -0,0 +1,102 @@
+close all
+
+a = 2;
+b = 10;
+A = 2;
+fc = 500;
+fDev = 50;
+tmin = -0.5;
+tmax = 0.5;
+L = 4096;
+Fe = L/(tmax-tmin); %Fe = 4096
+Te = 1/Fe;
+t = linspace(tmin,tmax,L);
+f = (-L/2:L/2-1)/(L/Fe);
+
+%% Initialisation des signaux
+m = a * cos(2*pi*b*t);
+pq = A * cos(2*pi*fc*t);
+M = fftshift(abs(fft(m,L)));
+P = fftshift(abs(fft(pq,L)));
+figure('Name','Init')
+subplot(221)
+plot(t,m)
+subplot(223)
+plot(t,pq)
+
+subplot(222)
+plot(f,M)
+subplot(224)
+plot(f,P)
+
+%% Demodulation FM/PM
+
+% a
+
+y = fmmod(m,fc,Fe,fDev);
+figure('Name','Modulation FM')
+plot(t,y)
+hold on
+plot(t,m)
+
+
+%%%%%detecteur d'envelope
+
+figure('Name','demodulation FM frequency')
+Ydem = diff(y); %derivateur
+Ydem = Ydem .* (Ydem>0); 
+TfYdem = fftshift(abs(fft(Ydem,L)));
+[B,A] = butter(5,0.01,"low");
+Sm1 = filter(B,A,Ydem);
+TfSm1 = fftshift(abs(fft(Sm1,L)));
+plot(f,TfSm1)
+%%%%%
+
+figure('Name','demodulation FM')
+plot(t(1:length(Ydem)),Ydem)
+hold on
+ydem =(Sm1-mean(Sm1));
+ydem = max(m)/max(ydem(L/2:end)) .* ydem; 
+plot(t(1:length(Ydem)),ydem)
+hold on 
+plot(t,m)
+
+
+
+
+
+%b FM demodulation
+UpZCi = @(v) find((v(1:end-1) <= 0 & v(2:end) > 0) | (v(1:end-1) >= 0 & v(2:end) < 0));	% Returns Up Zero-Crossing Indices
+
+
+figure('Name','FM demodulation')
+a=UpZCi(y);
+pulses = zeros(1,L);
+pulses(a)=1;
+stem(t,pulses)
+hold on
+plot(t,y)
+
+Tfpulses = fftshift(abs(fft(pulses,L)));
+[B,A] = butter(5,0.01,"low");
+Sm1 = filter(B,A,pulses);
+hold on
+plot(t,Sm1-mean(Sm1),'b')
+
+%c  PM demodulation
+
+y = pmmod(m,500,Fe,5);
+
+figure('Name','PM demodulation')
+a=UpZCi(y);
+pulses = zeros(1,L);
+pulses(a)=1;
+stem(t,pulses)
+hold on
+plot(t,y)
+
+Tfpulses = fftshift(abs(fft(pulses,L)));
+[B,A] = butter(5,0.01,"low");
+Sm1 = filter(B,A,pulses);
+hold on
+plot(t,Sm1-mean(Sm1),'b')
